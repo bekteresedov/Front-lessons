a4a5704cd7af7f754240ca467eaab9113b33722a
Bekter <besedov347@gmail.com>
2023-12-12 20:31:23 +0300

    lesson 2 added

---
+++ js/lesson2/index.js
@@ -0,0 +1,37 @@
+// var bosqab = prompt(" Deyeri daxil ele:");
+// console.log(typeof bosqab);
+// var int = Number(bosqab);
+// console.log(int);
+// console.log(typeof int);
+
+var name = "Huseyn";
+var age = 21;
+// if(name.length>3 &&age>18 &&name.length<20&&age<50)
+
+// if(name.length>3 ||age>18)
+// if(!age==3)
+
+// var alma =
+//   (age > 18 && age < 20)
+//     ? "Xos geldin"
+//     : age > 50
+//     ? "Qocalmisan Day Day"
+//     : "Xos Gelmedin";
+
+// var bosqab = "";
+// if (age > 18) {
+//   bosqab = "Xos geldin";
+// } else {
+//   bosqab = "Xosgelmedin";
+// }
+
+// console.log(alma);
+// console.log(bosqab);
+
+// console.log(confirm("Are you sure you want to"));
+
+// confirm("yasiniz 18 den boyukdur?");
+
+var alma = false;
+if (alma) console.log("get Almani ye");
+else console.log("get Almani yeme");
