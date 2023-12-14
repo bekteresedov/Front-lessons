b6f423bc3622b4b901f6ca9d7082143119c20703
Bekter <besedov347@gmail.com>
2023-12-14 20:42:15 +0300

    lesson3 added

---
+++ js/lesson3/index.js
@@ -0,0 +1,33 @@
+// var reqem = 2;
+// switch (reqem) {
+//   case 1:
+//     {
+//       console.log(1);
+//     }
+//     break;
+//   case 2: {
+//     console.log(2);
+//   }
+
+//   default:
+//     console.log(0);
+//     break;
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   //   console.log(i); 0 1 9
+// }
+
+// var i = 5;
+// while (i <= 15) {
+//   console.log(i);
+//   i *= 2;
+// }
+// console.log(i);
+// for (var i = 20; i > 5; i--) {}
+
+// var alma = 50;
+// do {
+//   console.log(alma);
+//   alma--;
+// } while (alma > 0);
