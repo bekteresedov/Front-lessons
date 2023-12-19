590716e0b4bf96c508b677f3d2b4c87acae6646c
Bekter <besedov347@gmail.com>
2023-12-19 20:40:41 +0300

    string methods added

---
+++ js/string-methods/index.js
@@ -0,0 +1,18 @@
+var alma = "Eilvin";
+// console.log(alma[1]);
+// ["E", "l", "v", "i", "n"];
+
+// console.log(alma.charAt(2)); bu funksiya bize simvolu qaytarir verilen indexe gore
+
+// console.log(alma.concat(" ", "Yusubov", " - ", "Frontend Developer"));  verilen arqumentleri birlesdirir
+
+// console.log(alma.endsWith("in"));
+// console.log(alma.startsWith("eEl"));
+
+// console.log(alma.includes("in"));
+
+// console.log(alma.indexOf("i"));
+
+// console.log(alma.repeat(3));
+
+console.log(alma.replaceAll("i", "o"));
