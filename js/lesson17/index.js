ac491f1d92f039cfd811ef2020a567acfdf6648e
Bekter <besedov347@gmail.com>
2024-01-30 22:07:10 +0300

    lesson 18 added

---
+++ js/lesson17/index.js
@@ -0,0 +1,50 @@
+var array = [4, 5, 7, 8];
+
+// var alma = (value, index) => {
+//   console.log("value: " + value);
+//   console.log("index: " + index);
+// };
+// array.forEach((value,index)=>{
+
+// });
+
+// var alma = ;
+
+// var newArray = array.filter((value) => value > 5);
+// console.log(newArray);
+
+// var findElement = array.find((value) => value > 5);
+// console.log(findElement);
+
+// var findIndex = array.findIndex((value) => value > 50);
+// console.log(findIndex);
+// input [[4,3,2],[2,4,6,8],[3,4,9]]=> output=>minus data length ? 4
+
+// var result = (heyva) =>
+//   heyva
+//     .map((element) => element.filter((value) => value < 0).length)
+//     .reduce((sum, el) => sum + el, 0);
+
+// console.log(result(array));
+// [[-1], [-1, -2, -3], [-8,-7]];
+
+// var array = [
+//   [40, -5],
+//   [-6, -7],
+// ];
+// var array3 = array[0].concat(array[1]);
+
+// console.log(array3);
+// console.log(array.some((element,index) => element < 0));
+// console.log(
+//   array.reduce(
+//     (sum, element, index) => (sum += element),
+//     "men Bekter menim qiymetlerim  "
+//   )
+// );
+
+// function test(a, b = 1) {}
+
+var alma = (array) => array.filter((el) => String(el).length % 2 == 0).length;
+
+console.log(alma([122, 232, 1234, 3223]));
